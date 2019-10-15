e09b5b44e6a30c6f1095fe67d1dc4a47a259b376
SumitSaw <49594771+SumitSaw@users.noreply.github.com>
2019-10-15 22:16:53 +0530

    Create V2_3__Multiple_Insert.sql

---
+++ src/main/resources/migrations/db/V2_3__Multiple_Insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee (emp_id,emp_name,city) VALUES (002, 'Cijo','Bangalore');
+INSERT INTO employee (emp_id,emp_name,city) VALUES (001, 'Padma','Bangalore');
+INSERT INTO employee (emp_id,emp_name,city) VALUES (001, 'Sandeep','Germany');
